c35405890262e8ba4fb32e59802d5c5682cf5c3d
QuentinChevaux <quentin.chevaux@gmail.com>
2021-11-24 14:02:25 +0100

    React Température

--- index.js
+++ index.js
@@ -1,39 +1,92 @@
-class DegreeCelsius extends React.Component {
+class Degree extends React.Component {
 
     constructor(props) {
 
         super(props)    //Initialise React Component
 
-        this.state = { degrecelius: "0" } //this.state état du composant,
+        this.state = { degrecelsius: "0", degreefahrenheit: "32", bout: "L'eau ne bout pas" } //this.state état du composant,
 
-        //je créé une variable nom dans le state et je lui affecte une chaine vide
-        this.chgInput = this.chgInput.bind(this)
+        this.change_celsius = this.change_celsius.bind(this) // variable avec chaine vide
+
+        this.change_fahrenheit = this.change_fahrenheit.bind(this)
     }
 
-    chgInput(event) {
+    change_celsius(event) {
+
+        this.setState({ degrecelsius: event.target.value })
+
+        this.setState({ degreefahrenheit: ( event.target.value * (9 / 5) + 32 )})
+
+        if (event.target.value >= 100) {
+
+            this.setState({ bout: "L'eau bout"})
+
+            console.log(event.target.value)
+
+        }
+
+    }
+
+    change_fahrenheit(event) {
 
         console.log(event.target.value)
 
-        this.setState({ nom: event.target.value })
+        this.setState({ degrecelsius: ( event.target.value - 32 ) * ( 5 / 9 )})
+
+        this.setState({ degreefahrenheit: event.target.value })
+
+        if (event.target.value >= 212) {
+
+            this.setState({ bout: "L'eau bout"})
+
+            console.log(event.target.value)
+
+        }
 
     }
+
     render() {
 
-        return <div>
+        return <div> 
 
-            <h1>Hello, {this.state.nom}</h1>
-            <input type="text" value={this.state.nom} onChange={this.chgInput} />
+            <div className="flex">
 
-                </div>
+            <h2>Entrez Votre valeur en °C : &nbsp;</h2>
+
+            <input type="number" value={this.state.degrecelsius} onChange={this.change_celsius} />
+
+            </div>
+
+            <br />
+            <br />
+
+            <div className="flex">            
+            <h2>Entrez Votre valeur en °F : &nbsp;</h2>
+
+            <input type="number" value={this.state.degreefahrenheit} onChange={this.change_fahrenheit} />
+            
+            </div>
+
+            <br />
+            <br />
+
+            <div>
+
+            <p> {this.state.bout} </p>
+
+            </div>
+
+        </div>
 
     }
 
 }
 
-let app = <div>
 
-    <Welcome></Welcome>
+let deg = <div>
+
+    <Degree></Degree>
 
 </div>
 
-ReactDOM.render(app, document.getElementById("div_c"))
+ReactDOM.render(deg, document.getElementById("deg"))
